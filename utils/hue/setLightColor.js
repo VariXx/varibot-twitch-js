bd171a0c8a414b631a322d00da6b5228490668a8
VariXx <varixx@gmail.com>
2020-09-10 20:45:13 -0500

    changed setLightColor to use xy value instead of searching for name

--- utils/hue/setLightColor.js
+++ utils/hue/setLightColor.js
@@ -2,14 +2,8 @@ const { bridgePut } = require('./bridgePut');
 
 async function setLightColor(bridgeIP, username, color, lightId) {
     let newColor = [0.692,0.308]; // red for error 
-    for(let key in colors) { 
-        if(key.toLowerCase() == color) {
-            newColor = colors[key];
-            break;
-        }
-    }
     let payload = {
-        xy: newColor
+        xy: color
     };
     await bridgePut(bridgeIP, username, `lights/${lightId}/state`, payload);
 }
